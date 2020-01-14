093afd571b3f70e32fb8ced5de9723c6ce99616f
hsanotra <sanotrah@sheridancollege.ca>
2020-01-13 17:39:08 -0800

    week2 push example

---
+++ src/testnow/TestNow.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testnow;
+
+/**
+ *
+ * @author HONEY
+ */
+public class TestNow {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+        System.out.println("Push operation test");
+    }
+    
+}
